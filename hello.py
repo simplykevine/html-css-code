dd0e5e0198218abd009696118d2358b592453b27
simplykevine <umutonikevine0000@gmail.com>
2023-10-31 10:07:57 +0200

    first commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+#you have to learn to insert comments to tell us what the pcode does or means
+x = 20
+y = 13
+
+print("enter an Operator")
+operator =input()
+
+#This is an example of a function
+def calculate():
+    print(f"The total is {x/0}")
+    return x/0
+    print("The total is {}".format(x+y))
+    print("The multiplication is "+str(x*y)+"")
+    #calculate()
